3ba8b3e3ef35f39673f550e5f79c0f98bc2279ba
John Ahlenhed <johnahlenhed@gmail.com>
2025-11-06 20:31:11 +0100

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+
+
+<footer>
+    <p>&copy; 2025 Kinforma. All rights reserved.</p>
+</footer>
+
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kinforma</title>
+    <link rel="stylesheet" href="/assets/styles.css">
+</head>
+<body>
+
+    <nav class="navbar">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+
+            <h1>Kinforma</h1>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
+
+        </nav>
+
+<script>
+
+    // Header scroll effect
+    console.log("Header loaded");
+    var nav = document.querySelector('.navbar');
+    function transparentNav() {
+        if (window.scrollY > 100) {
+            nav.classList.add('navbar-scrolled');
+        } else {
+            nav.classList.remove('navbar-scrolled');
+        }
+    }
+    window.addEventListener('scroll', transparentNav);
+</script>
