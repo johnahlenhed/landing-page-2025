52e144cbc9a6efdb1a2999c58064db6ab2797859
Your Name <laura.kotlinska.96@gmail.com>
2025-11-10 14:37:55 +0100

    Adds container for the 3d model and buttons to change the color

---
+++ includes/3d-model.php
@@ -0,0 +1,7 @@
+<figure id="model-container"></figure>
+
+<div class="part-textures">
+  <button class="model-color" data-part="rygg" data-texture="rygg_Base_color.jpg"></button>
+  <button class="model-color blue" data-part="rygg" data-texture="rygg_Base_color2.jpg"></button>
+  <button class="model-color red" data-part="rygg" data-texture="rygg_Base_color3.jpg"></button>
+</div>
