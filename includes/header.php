0c2a0d58051da13c444a7b67f841f85d172c303b
John Ahlenhed <johnahlenhed@gmail.com>
2025-11-13 16:16:05 +0100

    mobile pop-up menu

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/assets/styles.css">
     <script type="module" src="../assets/model.js"></script>
     <script type="importmap">
-    {
+        {
         "imports": {
           "three": "https://cdn.jsdelivr.net/npm/three@v0.181.0/build/three.module.js",
           "three/addons/": "https://cdn.jsdelivr.net/npm/three@v0.181.0/examples/jsm/"
@@ -20,17 +20,50 @@
 <body>
 
     <nav class="navbar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
+
+        <div class="menu-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+        </div>
 
         <h1>Kinforma</h1>
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-        </svg>
-
+        <div class="cart-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
+        </div>
     </nav>
+
+    <menu class="mobile-menu">
+
+        <div class="mobile-menu-top">
+            <img src="/assets/images/sweden-flag.svg" alt="swedish flag icon">
+            <img src="/assets/images/kinforma-logo-light.png" alt="Kinforma logo">
+            <div class="menu-icon-close">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </div>
+        </div>
+
+        <div class="menu-link-container">
+            <ul>
+                <li><a>Butik</a></li>
+                <li><a>Om oss</a></li>
+                <li><a>Kontakt</a></li>
+                <li><a>Hållbarhet</a></li>
+            </ul>
+        </div>
+
+        <div class="menu-user-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+            <p>Logga in</p>
+        </div>
+    </menu>
 
     <div class="video-container">
         <iframe
@@ -69,3 +102,5 @@
         }
         window.addEventListener('scroll', transparentNav);
     </script>
+
+    <script src="/assets/mobile-menu.js"></script>
