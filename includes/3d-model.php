c852bfd93f24ddc38dde81ef91db536cf5adf9ef
Your Name <laura.kotlinska.96@gmail.com>
2025-11-13 14:25:41 +0100

    Gigantic changes to 3d model - new lighting, color changes with hex instead of jpgs, buttons for the color changing

--- includes/3d-model.php
+++ includes/3d-model.php
@@ -1,9 +1,15 @@
-<figure id="model-container"></figure>
-
-<div class="part-textures">
-
-  <button class="model-color" data-part="rygg" data-texture="rygg_Base_color.jpg"></button>
-  <button class="model-color blue" data-part="rygg" data-texture="rygg_Base_color2.jpg"></button>
-  <button class="model-color red" data-part="rygg" data-texture="rygg_Base_color3.jpg"></button>
-  <button class="model-color" data-part="rygg" data-texture="rygg_Base_color4.jpg">test</button>
-</div>
+<section>
+  <h2>Personlig Design</h2>
+  <p>Skapa din egen design av vår fåtölj – personlig, unik och helt i din stil.</p>
+  <figure id="model-container"></figure>
+  <div id="color-options">
+    <button class="model-color" data-color="#000000">Black</button>
+    <button class="model-color" data-color="#ffffff">Original</button>
+    <button class="model-color" data-color="#DEDBC0">Beige</button>
+    <button class="model-color" data-color="#AC7F5E">Brown</button>
+    <button class="model-color" data-color="#B9B9B9">Grey</button>
+    <button class="model-color" data-color="#810C0E">Red</button>
+    <button class="model-color" data-color="#394384">Blue</button>
+    <button class="model-color" data-color="#616E42">Green</button>
+  </div>
+</section>
