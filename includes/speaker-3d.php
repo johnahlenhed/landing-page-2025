7d9b2b432baca7e57e5b025b5b40210a2f413583
John Ahlenhed <johnahlenhed@gmail.com>
2025-11-23 15:44:12 +0100

    Speaker 3D functions and textures

--- includes/speaker-3d.php
+++ includes/speaker-3d.php
@@ -1,6 +1,4 @@
 <section class="speaker-section">
     <div id="speaker-3d"></div>
     <button id="explode-button">Explode!</button>
-
-    <input type="range" min="0" max="100" value="50" class="explosion-slider">
 </section>
