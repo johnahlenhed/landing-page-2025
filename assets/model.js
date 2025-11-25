46509b31d7ef591736f340ac079f7853e7c4c826
Your Name <laura.kotlinska.96@gmail.com>
2025-11-25 17:01:02 +0100

    Loads new model with dynamic materials

--- assets/model.js
+++ assets/model.js
@@ -1,10 +1,8 @@
 import * as THREE from "three";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
-import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { FXAAPass } from "three/addons/postprocessing/FXAAPass.js";
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
-import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 
 // ---- Container for the 3d model
 const container = document.getElementById("model-container");
@@ -33,7 +31,7 @@ const camera = new THREE.PerspectiveCamera(
   1,
   500
 );
-camera.position.set(0, 2, 30);
+camera.position.set(6, 3, 10);
 
 // ---- Camera controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -58,90 +56,117 @@ const rimLight = new THREE.DirectionalLight(0xffffff, 0.9);
 rimLight.position.set(-3, 5, -5);
 scene.add(rimLight);
 
-// ---- Material references
-const textureLoader = new THREE.TextureLoader();
-
-const createMaterial = (part) =>
-  new THREE.MeshStandardMaterial({
-    map: textureLoader.load(
-      `../assets/3d-model/texturer_test1/${part}_Base_color.jpg`
-    ),
-    normalMap: textureLoader.load(
-      `../assets/3d-model/texturer_test1/${part}_Normal.jpg`
-    ),
-    roughnessMap: textureLoader.load(
-      `../assets/3d-model/texturer_test1/${part}_Roughness.jpg`
-    ),
-    metalnessMap: textureLoader.load(
-      `../assets/3d-model/texturer_test1/${part}_Metallic.jpg`
-    ),
-    bumpMap: textureLoader.load(
-      `../assets/3d-model/texturer_test1/${part}_Height.jpg`
-    ),
-    roughness: 1.0,
-    metalness: 0.8,
-  });
-
-// ---- Material objects
-const materials = {
-  seat: null,
-  sidor: null,
-  kudde: null,
+let globalTextureParams = {
+  kudde: {
+    color: "beige",
+    part: "kudde",
+    meshName: "kudde",
+  },
+  seat: {
+    color: "ek",
+    part: "chair",
+    meshName: "seat",
+  },
+  sidor: {
+    color: "ek",
+    part: "chair",
+    meshName: "sidor",
+  },
 };
 
-// ---- Loading model with textures
+// ---- Textures
+const textureLoader = new THREE.TextureLoader();
+
+function loadPartMaterial(dataColor, dataPart, dataMesh) {
+  const base = `../assets/3d-model/chair_texturer/${dataPart}_${dataColor}/${dataMesh}`;
+
+  return new THREE.MeshStandardMaterial({
+    name: dataMesh,
+    map: textureLoader.load(`${base}_Base_color.png`),
+    normalMap: textureLoader.load(`${base}_Normal.png`),
+    roughnessMap: textureLoader.load(`${base}_Roughness.png`),
+    metalnessMap: textureLoader.load(`${base}_Metallic.png`),
+    bumpMap: textureLoader.load(`${base}_Height.png`),
+    roughness: 2.0,
+    metalness: 0.9,
+  });
+}
+
+// ---- Assigning material to buttons clicks
+function changeModelTextures(color, part, meshList) {
+  meshList.forEach((meshName) => {
+    globalTextureParams = {
+      ...globalTextureParams,
+      ...{
+        [meshName]: {
+          color,
+          part,
+          meshName,
+        }
+      }
+    }
+    setChairMaterials(scene, globalTextureParams);
+  });
+
+}
+
+// ---- Funtionality for buttons
+
+document.querySelectorAll(".swatch").forEach((btn) => {
+  btn.style.backgroundColor = btn.getAttribute("button-color");
+
+  btn.addEventListener("click", () => {
+    const color = btn.dataset.color; // brun, ek, svart
+    const part = btn.dataset.part; // chair OR kudde
+    const meshList = btn.dataset.mesh.split(","); // "seat,sidor" splitting into array
+
+    changeModelTextures(color, part, meshList);
+  });
+});
+
+function setChairMaterials(
+  object3d,
+  textureParams = globalTextureParams,
+) {
+  object3d.traverse((child) => {
+    if (!child.isMesh) return;
+
+    if (!Array.isArray(child.material) && child.name === "kudde" || child.material.name === "kudde") {
+      child.material = loadPartMaterial(
+        textureParams.kudde.color,
+        textureParams.kudde.part,
+        textureParams.kudde.meshName
+      );
+      child.material.needsUpdate = true;
+    } else {
+      child.material.forEach((material, index) => {
+        child.material[index] = loadPartMaterial(
+          textureParams[material.name].color,
+          textureParams[material.name].part,
+          textureParams[material.name].meshName
+        );
+        child.material[index].needsUpdate = true;
+      });
+    }
+  });
+}
+
 const objLoader = new OBJLoader();
 objLoader.setPath("../assets/3d-model/");
 
 objLoader.load("chair_uv_ek.obj", (object) => {
-  object.position.set(0, -6, 0);
-  object.rotation.y = -Math.PI / 1.5;
+  object.position.set(0, -0.5, 0);
+  object.rotation.x = -Math.PI / 2;
+  object.rotation.z = Math.PI;
 
-  object.traverse((child) => {
-    if (!child.isMesh) return;
-
-    const name = child.name.toLowerCase();
-
-    if (name.includes("seat")) {
-      materials.seat = createMaterial("seat");
-      child.material = materials.seat;
-    }
-
-    if (name.includes("sidor")) {
-      materials.sidor = createMaterial("sidor");
-      child.material = materials.sidor;
-    }
-
-    if (name.includes("kudde")) {
-      materials.kudde = createMaterial("kudde");
-      child.material = materials.kudde;
-    }
-  });
+  setChairMaterials(object);
 
   scene.add(object);
 });
 
-// ---- Apply color tint to materials
-function applyColor(parts, hexColor) {
-  parts.forEach((part) => {
-    const mat = materials[part];
-    if (!mat) return;
-
-    mat.color = new THREE.Color(hexColor);
-    mat.needsUpdate = true;
-  });
-}
-
-// ---- Applies color to model depending on the value
-document.querySelectorAll("#color-options button").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const hex = btn.getAttribute("data-color");
-    applyColor(["seat", "rygg"], hex);
-  });
-});
 // ---- Changes the color of the button to the same color as given value
 document.querySelectorAll(".swatch").forEach((btn) => {
-  const color = btn.getAttribute("data-color");
+  const color = btn.getAttribute("button-color");
   btn.style.backgroundColor = color;
 });
 
