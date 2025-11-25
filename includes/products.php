8ac48ffc50f0d5760fb056f26a7b5bf0b3fb0927
Your Name <laura.kotlinska.96@gmail.com>
2025-11-25 17:47:00 +0100

    Short hyphen for "3d-printade"

--- includes/products.php
+++ includes/products.php
@@ -6,7 +6,7 @@ require("content.php");
         <div class="products-intro">
             <p class="product-season">Höst 2026</p>
             <h2>Marstrand</h2>
-            <p>Upptäck vår senaste kollektion av 3D–printade möbler. Tillverkade i Göteborg av återvunnet fiskenät från Marstrand.</p>
+            <p>Upptäck vår senaste kollektion av 3D-printade möbler. Tillverkade i Göteborg av återvunnet fiskenät från Marstrand.</p>
         </div>
         <div>
             <button class="cta-button product-button">
